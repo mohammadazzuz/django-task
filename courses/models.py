0fc69ae0d18bdf147864dc73009a3bccb2ec12e4
mohammadazzuz <112818173+mohammadazzuz@users.noreply.github.com>
2023-01-26 15:38:47 +0100

    Fix db_collation attribute of LanguageField

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+from languages.fields import LanguageField
 
 # Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=1000)
+    subtitle = models.TextField(max_length=5000)
+    author = models.ForeignKey(User,related_name='course_author',on_delete=models.CASCADE)
+    date_modified = models.DateTimeField(auto_now=True)
+    language = LanguageField()
+    course_content = models.TextField(max_length=100000000000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    def __str__(self):
+        return self.title
+    
+
